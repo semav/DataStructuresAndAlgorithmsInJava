146dec972b78dcddabb3fe70cd8d762674cc5890
alexander <1semav@gmail.com>
2019-07-14 22:39:04 +0300

    Analysis of Algorithms

---
+++ src/semav/chapter4/PrefixAverage.java
@@ -0,0 +1,17 @@
+package semav.chapter4;
+
+public final class PrefixAverage {
+    private final int[] numbers;
+
+    public PrefixAverage(int[] numbers) {
+        this.numbers = numbers.clone();
+    }
+
+    public int[] prefixAveragesQuadratic(){
+        return null;
+    }
+
+    public int[] prefixAveragesLinear(){
+        return null;
+    }
+}
