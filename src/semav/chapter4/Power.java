f6f7bc7a8d15417e4965890cf97b5fd831a8bec7
alexander <1semav@gmail.com>
2019-07-16 22:02:15 +0300

    Analysis of Algorithms

--- src/semav/chapter4/Power.java
+++ src/semav/chapter4/Power.java
@@ -1,17 +1,34 @@
 package semav.chapter4;
 
 public final class Power {
-    private final float number;
+    private final int number;
 
-    public Power(float number) {
+    public Power(int number) {
         this.number = number;
     }
 
     public float get(int power){
-        float result = 1;
+        int result = 1;
         for (int i = 0; i < power; i++){
             result *= number;
         }
         return result;
+    }
+
+    public int getWithRecursion(int power){
+        return power(number, power);
+    }
+
+    private int power(int n, int power){
+        if (power == 0){
+            return 1;
+        } else if (power % 2 == 0){
+            int tmp = power(n, power / 2);
+            return tmp * tmp;
+        }
+        else{
+            int tmp = power(n, (power - 1) / 2);
+            return tmp * tmp * n;
+        }
     }
 }
--- src/semav/chapter4/PowerTests.java
+++ src/semav/chapter4/PowerTests.java
@@ -7,14 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public final class PowerTests {
     @Test
-    public void shouldReturnPower1(){
+    public void shouldReturnPower(){
         Power power = new Power(2);
         assertEquals(1024, power.get(10));
     }
 
     @Test
-    public void shouldReturnPower2(){
+    public void shouldReturnEvenPowerWithRecursion(){
         Power power = new Power(2);
-        assertEquals(1024, power.get(10));
+        assertEquals(1024, power.getWithRecursion(10));
+    }
+
+    @Test
+    public void shouldReturnOddPowerNumberWithRecursion(){
+        Power power = new Power(2);
+        assertEquals(2048, power.getWithRecursion(11));
     }
 }
